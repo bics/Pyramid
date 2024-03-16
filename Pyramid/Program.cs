51b96410e3a21e7dc57740a65b833be9c7c79d04
PC\Bics <bicsi94@gmail.com>
2024-03-16 17:11:21 +0000

    P-m-Initial

---
+++ Pyramid/Program.cs
@@ -0,0 +1,66 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Input how many rows:");
+        string? input = Console.ReadLine();
+
+        Writer(input);
+
+    }
+
+    private static void Writer(string? rows)
+    {
+        if (Int32.TryParse(rows, out int rowNumber))
+        {
+            Console.WriteLine("Input was a number: {0}", rowNumber);
+
+            WriterHelper(rowNumber);
+
+            for (int i = 1; i < rowNumber; i++)
+            {
+                WriterHelper(rowNumber - i, rowNumber + i);
+                Console.WriteLine();
+            }            
+        }
+    }
+
+    private static void WriterHelper(int firstPos, int? lastPos = null)
+    {
+        if (lastPos == null)
+        {
+            for (int i = 1; i <= firstPos; i++)
+            {
+                if (i == firstPos)
+                {
+                    Console.Write("^");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int i = 1; i <= lastPos; i++)
+            {
+                if (i == firstPos)
+                {
+                    Console.Write("/");
+                }
+                else if (i == lastPos)
+                {
+                    Console.Write(Convert.ToChar(92));
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+
+    }
+}
